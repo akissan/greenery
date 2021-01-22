25949d9c5531501ece66d8178b3e4edea5eb1267
Akissan <astellakissan@gmail.com>
2021-01-23 01:00:42 +0300

    shiftaligner 2.0

--- dev/shiftaligner/sketch.js
+++ dev/shiftaligner/sketch.js
@@ -1,8 +1,8 @@
-import initButtons from "./cmps/buttons";
-
 let grid = true;
 
 let loadedSprite = null;
+let loadedShadow = null;
+
 let shift;
 let collision_box;
 
@@ -12,60 +12,110 @@ let tb = 32;
 let tile = tb * SF;
 let npx = 1.0 / tb;
 let px = 1.0 / tile;
-const shiftToPx = (v) => p5.Vector.mult(v, tile);
+const shiftToPx = (v) => ({ x: v.x * tile, y: v.y * tile });
 
 let dropzone;
 let slider;
 
 let CENT = [0, 0];
 
-hightDrop = () => dropzone.style("background-color", "orange");
-unhightDrop = () => dropzone.style("background-color", "#363d42");
+toggleGrid = () => {
+  grid = !grid;
+};
 
-const initDropzone = () => {
-  let dz = select("#spriteinput");
-  dz.drop(gotFile, unhightDrop);
-  dz.dragOver(hightDrop);
-  dz.dragLeave(unhightDrop);
+const buttons = {
+  Grid: new ButtonInfo([64, 84], toggleGrid),
+  "x-": new ButtonInfo([128, 84], () => adjustShift([-1 * npx, 0])),
+  "x+": new ButtonInfo([192, 84], () => adjustShift([1 * npx, 0])),
+  "y+": new ButtonInfo([128, 84 + 48], () => adjustShift([0, 1 * npx])),
+  "y-": new ButtonInfo([192, 84 + 48], () => adjustShift([0, -1 * npx])),
+};
+
+hightDrop = (dz) => dz.style("background-color", "orange");
+unhightDrop = (dz) => dz.style("background-color", "#363d42");
+
+const initDropzone = (tag, onDrop) => {
+  let dz = select(tag);
+  if (dz) {
+    dz.drop(onDrop, () => unhightDrop(dz));
+    dz.dragOver(() => hightDrop(dz));
+    dz.dragLeave(() => unhightDrop(dz));
+  }
   return dz;
 };
 
 const initSlider = (pos) => {
   let slide = createSlider(0, 4, 1, 1);
   slide.position(...pos);
-  return;
+  return slide;
 };
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  dropzone = initDropzone();
-  slider = initSlider(64, 128);
+  dropzone = initDropzone(
+    "#spriteinput",
+    (e) => (loadedSprite = gotFile(e, "Lsprite"))
+  );
+  dropzone = initDropzone(
+    "#shadowinput",
+    (e) => (loadedShadow = gotFile(e, "Ssprite"))
+  );
 
-  initButtons();
+  slider = initSlider([64, 512]);
 
-  shift = createVector(-0.5, 0.5);
+  initButtons(buttons);
+
+  shift = { x: 0, y: 0 }; //0.5 = 16px
+  let inp_h = createInput("");
+  inp_h.input((e) => (shift.x = inp_h.value()));
+  inp_h.position(64, 512 - 128);
+  let inp_v = createInput("");
+  inp_v.input((e) => (shift.y = inp_v.value()));
+  inp_v.position(64, 512 - 128 + 48);
 
   collision_box = [
     [-0.35, -0.35],
     [0.35, 0.35],
   ];
+
+  CENT[0] = Math.floor((width * 0.5) / tile);
+  CENT[1] = Math.floor((height * 0.5) / tile);
+
   noSmooth();
 
-  loadedSprite = getItem("Lsprite");
   console.log(loadedSprite);
+
+  loadedSprite = getSprite(getItem("Lsprite") || null);
+  loadedShadow = getSprite(getItem("Ssprite") || null);
 }
 
-adjustShift = (v) => shift.add(v);
+adjustShift = (v) =>
+  (shift = {
+    x: Number(shift.x) + Number(v[0]),
+    y: Number(shift.y) + Number(v[1]),
+  });
 
-gotFile = (file) => {
-  loadedSprite = createImg(file.data);
-  storeItem("Lsprite", loadedSprite);
-  loadedSprite.hide();
+const getSprite = (data) => {
+  if (data) {
+    let tmp = createImg(data);
+    tmp.hide();
+    return tmp;
+  }
+  return null;
 };
 
-toggleGrid = () => {
-  grid = !grid;
+function mouseClicked() {
+  console.log(mouseX, mouseY);
+  if (mouseX > 256) {
+    setCenter(mouseX / tile, mouseY / tile);
+  }
+}
+
+const gotFile = (file, cookie) => {
+  let tmp = getSprite(file.data);
+  if (tmp) storeItem(cookie, file.data);
+  return tmp;
 };
 
 drawBounds = (bs) => {
@@ -89,14 +139,27 @@ drawBounds = (bs) => {
   fill(255);
 };
 
+function setCenter(tX, tY) {
+  (CENT[0] = Math.floor(tX)), (CENT[1] = Math.floor(tY));
+}
+
+function drawSprite(sprite, shift) {
+  fill(150);
+  //rect(CENT[0] * tile, CENT[1] * tile, tile, tile);
+  image(
+    sprite,
+    CENT[0] * tile + shiftToPx(shift).x - sprite.width * 0.5 * SF + tile * 0.5,
+    CENT[1] * tile + shiftToPx(shift).y - sprite.height * 0.5 * SF + tile * 0.5,
+    sprite.width * SF,
+    sprite.height * SF
+  );
+}
+
 function draw() {
   SF = 0.5 * (2 << slider.value());
 
   tile = tb * SF;
   px = 1.0 / tile;
-
-  CENT[0] = Math.floor((width * 0.5) / tile);
-  CENT[1] = Math.floor((height * 0.5) / tile);
 
   background(240);
   fill(0);
@@ -112,16 +175,12 @@ function draw() {
   fill(255);
   rect(0, 0, 256, height);
 
+  if (loadedShadow) {
+    drawSprite(loadedShadow, shift);
+  }
+
   if (loadedSprite) {
-    fill(150);
-    rect(CENT[0] * tile, CENT[1] * tile, tile, tile);
-    image(
-      loadedSprite,
-      CENT[0] * tile + shiftToPx(shift).x,
-      CENT[1] * tile + shiftToPx(shift).y,
-      20,
-      20
-    );
+    drawSprite(loadedSprite, createVector(0, 0));
     //console.log("DRAW");
     drawBounds(collision_box);
   }
